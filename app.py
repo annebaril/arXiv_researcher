5712c4c551a33bdaea775306e8b07efae3d9d756
Celia ZHANG <celiazhang349@gmail.com>
2025-04-03 10:22:30 +0000

    debug the vectorstore problem for tool functions

--- app.py
+++ app.py
@@ -3,24 +3,19 @@ from dotenv import load_dotenv
 from langchain_huggingface import HuggingFaceEmbeddings, HuggingFaceEndpoint
 from langchain_chroma import Chroma
 from langchain import hub
+import chromadb
 
 load_dotenv()
 
-from arxivsearcher.load_chroma import download_directory_from_gcs
-from arxivsearcher.retrieval import search_articles
 from arxivsearcher.llm_agent import create_agent
 
-
-BUCKET_NAME = os.getenv("BUCKET_NAME")
-GCS_PERSIST_PATH = os.getenv("GCS_PERSIST_PATH")
-LOCAL_PERSIST_PATH = os.getenv("LOCAL_PERSIST_PATH")
-
-# load chromadb to local from gcs
-download_directory_from_gcs(GCS_PERSIST_PATH, LOCAL_PERSIST_PATH, BUCKET_NAME)
+CHROMADB_HOST = os.getenv("CHROMADB_HOST")
+chroma_client = chromadb.HttpClient(host=CHROMADB_HOST, port=8000)
+chroma_client.heartbeat()
 
 EMBEDDING_MODEL = os.getenv("EMBEDDING_MODEL")
 embeddings = HuggingFaceEmbeddings(model_name=EMBEDDING_MODEL)
-vectorstore = Chroma(persist_directory=LOCAL_PERSIST_PATH, embedding_function=embeddings)
+vectorstore = Chroma(embedding_function=embeddings, client=chroma_client)
 
 LLM_MODEL = os.getenv("LLM_MODEL")
 HUGGINGFACEHUB_API_TOKEN = os.getenv("HUGGINGFACE_TOKEN")
@@ -30,11 +25,11 @@ llm = HuggingFaceEndpoint(
     huggingfacehub_api_token=HUGGINGFACEHUB_API_TOKEN,
     task="text-generation"
 )
-tools = [search_articles]
+
 AGENT_PROMPT = os.getenv("AGENT_PROMPT")
 prompt = hub.pull(AGENT_PROMPT)
 
-agent_executor = create_agent(llm, tools, prompt)
+agent_executor = create_agent(vectorstore, llm, prompt)
 
 def main():
     print("Welcome to arXiv AI searcher.")
@@ -44,3 +39,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+    #show me some articles on machine learning
